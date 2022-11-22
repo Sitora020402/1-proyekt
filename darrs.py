ff8e857846861f3668e28735ed81a1a1e8c86c76
Sitora020402 <118831338+Sitora020402@users.noreply.github.com>
2022-11-22 20:06:40 +0500

    Add files via upload

---
+++ darrs.py
@@ -0,0 +1,74 @@
+# import time
+
+
+# def ikkinchi():
+#     for x in range(20):
+#         time.sleep(1)
+#         print(x
+
+
+# import time
+# a=0
+# while True:
+#     a +=1
+#     parol = input("Parol kiriting:")
+#     if parol == "12345":
+#         print("katta xatolik:")
+#         break
+#     elif a ==3:
+#         for x in range (1,10):
+#             time.sleep(1)
+#             print(x)
+#         print("xatocha")
+
+#     elif a ==5:
+#         print("xato bor:")
+#         for x in range(1 ,30):
+#             time.sleep(1)
+#             print(x)
+#         print("xato:")
+#     else:
+#         print('xato')    
+
+
+# a = float(input("son kiriting:"))
+
+# if a  % 2 ==0:
+#     print("juft son") 
+# elif a % 2 ==1:
+#     print("toq son:")       
+# else:
+#     print("toq son xam emas juft xam emas: ")     
+
+
+# while True: 
+#     parol = input = ("parol kiriting:")
+#     login = input = ("login kiriting:")
+#     if parol =="1234":
+#         print("dasturga kirdingiz:")
+    
+#         break
+#     else:
+#         print("xato:") 
+
+
+
+
+
+# sitorabonu#2002
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
